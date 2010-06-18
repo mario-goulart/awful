7c93ab8a2691f1d70870a1383fd27399ac25e6c5
Mario Domenech Goulart <mario.goulart@gmail.com>
2010-06-18 10:23:53 +0000

    awful (trunk): addes tests/update-targets.scm to test the update of multiple targets.

---
+++ tests/update-targets.scm
@@ -0,0 +1,19 @@
+#!/usr/bin/awful
+
+(use awful)
+
+(enable-ajax #t)
+
+(define-page (main-page-path)
+  (lambda ()
+
+    (ajax "foo" 'foo 'click
+          (lambda ()
+            '((a . 1) (b . 2) (c . 3)))
+          update-targets: '(a b c))
+
+    (<div>
+     (link "#" "foo" id: "foo")
+     (<div> id: "a")
+     (<div> id: "b")
+     (<div> id: "c"))))
