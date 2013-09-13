mario-goulart/awful

commit 03e4e6b9ca2378928fa35c5b8b68738561010aa2

check to make sure redirect-to-login-page always generates html
Signed-off-by: Mario Domenech Goulart <mario.goulart@gmail.com>

awful.scm
@@ -764,15 +764,16 @@
     ((page-exception-message) exn)))
 
 (define-inline (redirect-to-login-page path)
-  ((page-template)
-   ""
-   headers: (<meta> http-equiv: "refresh"
-                    content: (++ "0;url=" (login-page-path)
-                                 "?reason=invalid-session&attempted-path=" path
-                                 "&user=" ($ 'user "")
-                                 (if (and (not (enable-session-cookie)) ($ 'sid))
-                                     (++ "&sid=" ($ 'sid))
-                                     "")))))
+  (let ((content ((page-template)
+                    ""
+                    headers: (<meta> http-equiv: "refresh"
+                                     content: (++ "0;url=" (login-page-path)
+                                                  "?reason=invalid-session&attempted-path=" path
+                                                  "&user=" ($ 'user "")
+                                                  (if (and (not (enable-session-cookie)) ($ 'sid))
+                                                    (++ "&sid=" ($ 'sid))
+                                                    ""))))))
+    (if sxml? ((sxml->html) content) content)))
 
 (define-inline (render-page contents path given-path no-javascript-compression ajax? sxml?)
   (let ((++* (if sxml? (lambda args (apply append (map list args))) ++))

commit 95492ecf275e7f3b6daafe6cb84137863323a74b

redirect-to-login-page: drop html-tags code (thanks to Thomas Hintz)
This fixes a bug introduced in 03e4e6b9ca2378928fa35c5b8b68738561010aa2

Thanks to Thomas Hintz for reporting that and suggesting removing
html-tags code.

awful.scm
@@ -764,16 +764,19 @@
     ((page-exception-message) exn)))
 
 (define-inline (redirect-to-login-page path)
-  (let ((content ((page-template)
-                    ""
-                    headers: (<meta> http-equiv: "refresh"
-                                     content: (++ "0;url=" (login-page-path)
-                                                  "?reason=invalid-session&attempted-path=" path
-                                                  "&user=" ($ 'user "")
-                                                  (if (and (not (enable-session-cookie)) ($ 'sid))
-                                                    (++ "&sid=" ($ 'sid))
-                                                    ""))))))
-    (if sxml? ((sxml->html) content) content)))
+  ((sxml->html)
+   `(html
+     (head
+      (meta (@ (http-equiv "refresh")
+               (content
+                ,(sprintf
+                  "0;url=~a?reason=invalid-session&attempted-path=~a&user=~a~a"
+                  (login-page-path)
+                  path
+                  ($ 'user "")
+                  (if (and (not (enable-session-cookie)) ($ 'sid))
+                      (string-append "&sid=" ($ 'sid))
+                      "")))))))))
 
 (define-inline (render-page contents path given-path no-javascript-compression ajax? sxml?)
   (let ((++* (if sxml? (lambda args (apply append (map list args))) ++))