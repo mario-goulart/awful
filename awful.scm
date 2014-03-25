mario-goulart/awful

commit 745db9ad688853455e4c2b69f5f692ad2897006a

Make awful-start properly honor the dev-mode? keyword parameter
Before this change, development-mode-actions would only be called from
load-apps.  So, if an application uses awful-start with dev-mode? set
to #t, but doesn't call load-apps, development-mode-actions would not
be called.

While at it, call development-mode-actions _before_ evaluating the
thunk for awful-start, so that the evaluated code can override the
settings made by development-mode-actions.

awful.scm
@@ -199,8 +199,7 @@
       str))
 
 (define (load-apps apps)
-  (for-each load apps)
-  (when (development-mode?) (development-mode-actions)))
+  (for-each load apps))
 
 (define (reload-apps apps)
   (reset-resources!)
@@ -270,7 +269,9 @@
 
 (define (awful-start thunk #!key dev-mode? port ip-address (use-fancy-web-repl? #t) privileged-code)
   (enable-web-repl-fancy-editor use-fancy-web-repl?)
-  (when dev-mode? (development-mode? #t))
+  (when dev-mode?
+    (development-mode? #t)
+    (development-mode-actions))
   (when port (server-port port))
   (when ip-address (server-bind-address ip-address))
   ;; if privileged-code is provided, it is loaded before switching

commit 81b20a0b04f8df33a2fedef54541c581b6310c78

Run development mode actions when reloading apps
Fixes a bug introduced by 745db9ad688853455e4c2b69f5f692ad2897006a

awful.scm
@@ -203,6 +203,8 @@
 
 (define (reload-apps apps)
   (reset-resources!)
+  (when (development-mode?)
+    (development-mode-actions))
   (load-apps apps))
 
 (define (define-reload-page)