mario-goulart/awful

commit 4f4bc103add5649a0907958f62bdcfc551721a34

Support C6

awful.scm
@@ -88,7 +88,7 @@
    ;; Special definitions
    (define slurp read-all))
 
-  (chicken-5
+  ((or chicken-5 chicken-6)
    ;; Built-in modules
    (import (chicken base)
            (chicken condition)
@@ -111,6 +111,7 @@
            (chicken type)
            (chicken string)
            (chicken condition))
+   (cond-expand (chicken-6 (import (scheme base))) else)
    (import srfi-14)
 
    ;; Eggs

awful-server.scm
@@ -33,7 +33,7 @@
    (declare (uses chicken-syntax))
    (use data-structures files irregex posix srfi-1 srfi-13)
    (use awful))
-  (chicken-5
+  ((or chicken-5 chicken-6)
    (import (chicken base)
            (chicken irregex)
            (chicken pathname)

tests/access-control.scm
@@ -3,7 +3,7 @@
 (cond-expand
   (chicken-4
    (use awful))
-  (chicken-5
+  ((or chicken-5 chicken-6)
    (import awful))
   (else
    (error "Unsupported CHICKEN version.")))

tests/ajax-increment.scm
@@ -1,7 +1,7 @@
 (cond-expand
   (chicken-4
    (use awful))
-  (chicken-5
+  ((or chicken-5 chicken-6)
    (import awful))
   (else
    (error "Unsupported CHICKEN version.")))

tests/ajax-link.scm
@@ -1,7 +1,7 @@
 (cond-expand
   (chicken-4
    (use awful posix))
-  (chicken-5
+  ((or chicken-5 chicken-6)
    (import (chicken time))
    (import awful))
   (else

tests/ajax-sxml.scm
@@ -1,7 +1,7 @@
 (cond-expand
   (chicken-4
    (use awful))
-  (chicken-5
+  ((or chicken-5 chicken-6)
    (import awful))
   (else
    (error "Unsupported CHICKEN version.")))

tests/ajax.scm
@@ -1,7 +1,7 @@
 (cond-expand
   (chicken-4
    (use awful))
-  (chicken-5
+  ((or chicken-5 chicken-6)
    (import (chicken string)
            (chicken time))
    (import awful))

tests/arc-challenge.scm
@@ -1,7 +1,7 @@
 (cond-expand
   (chicken-4
    (use awful))
-  (chicken-5
+  ((or chicken-5 chicken-6)
    (import awful))
   (else
    (error "Unsupported CHICKEN version.")))

commit ebabe6b098cfd5cef022d5c1ca19a229c4c51011

Fix cond-expand's else clause
Issue introduced in 4f4bc103add5.

awful.scm
@@ -111,7 +111,7 @@
            (chicken type)
            (chicken string)
            (chicken condition))
-   (cond-expand (chicken-6 (import (scheme base))) else)
+   (cond-expand (chicken-6 (import (scheme base))) (else))
    (import srfi-14)
 
    ;; Eggs